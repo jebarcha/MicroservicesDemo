81d001e443cd6c280f17a196bf366dbc6f671a2a
jebarcha <jebarcha@hotmail.com>
2021-10-28 17:34:14 -0500

    Home and Details pages

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -17,7 +17,7 @@ namespace Mango.Services.ProductAPI.Controllers
             this._response = new ResponseDto();
         }
 
-        [Authorize]
+        //[Authorize]
         [HttpGet]
         public async Task<object> Get()
         {
@@ -35,7 +35,7 @@ namespace Mango.Services.ProductAPI.Controllers
         }
 
         [HttpGet]
-        [Authorize]
+        //[Authorize]
         [Route("{id}")]
         public async Task<object> Get(int id)
         {
