2f618a080a26aa29d621673580dc7f3b1932e85b
jebarcha <jebarcha@hotmail.com>
2021-11-16 15:49:19 -0600

    Checkout functionality

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -1,4 +1,5 @@
-﻿using Mango.Services.ShoppingCartAPI.Models.Dto;
+﻿using Mango.Services.ShoppingCartAPI.Messages;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
 using Mango.Services.ShoppingCartAPI.Repository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -114,7 +115,27 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
             return _response;
         }
 
-
+        [HttpPost("Checkout")]
+        public async Task<object> Checkout(CheckoutHeaderDto checkoutHeader)
+        {
+            try
+            {
+                CartDto cartDto = await _cartRepository.GetCartByUserId(checkoutHeader.UserId);
+                if (cartDto == null)
+                {
+                    return BadRequest();
+                }
+                checkoutHeader.CartDetails = cartDto.CartDetails;
+                //logic to add message to process order.
+                
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.Message };
+            }
+            return _response;
+        }
 
 
         [HttpPost("ClearCart")]
--- Mango.Web/Services/IServices/ICartService.cs
+++ Mango.Web/Services/IServices/ICartService.cs
@@ -10,6 +10,7 @@ namespace Mango.Web.Services.IServices
         Task<T> RemoveFromCartAsync<T>(int cartId, string token = null);
         Task<T> ApplyCouponAsync<T>(CartDto cartDto, string token = null);
         Task<T> RemoveCouponAsync<T>(string userId, string token = null);
+        Task<T> Checkout<T>(CartHeaderDto cartHeader, string token = null);
         Task<T> ClearCartAsync<T>(string userId, string token = null);
     }
 }
