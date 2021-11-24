bfc48aa2b34c850b7cba6d40a4e640bdbb84c8d8
jebarcha <jebarcha@hotmail.com>
2021-11-24 12:49:09 -0600

    Sync microservice communication

--- Mango.Services.ShoppingCartAPI/Models/Dto/ResponseDto.cs
+++ Mango.Services.ShoppingCartAPI/Models/Dto/ResponseDto.cs
@@ -3,8 +3,8 @@
     public class ResponseDto
     {
         public bool IsSuccess { get; set; } = true;
-        public object Result { get; set; } = "";
+        public object Result { get; set; }  // = "";
         public string DisplayMessage { get; set; } = "";
-        public List<string> ErrorMessages { get; set; } = new List<string>();
+        public List<string> ErrorMessages { get; set; } // = new List<string>();
     }
 }
--- Mango.Services.ShoppingCartAPI/Program.cs
+++ Mango.Services.ShoppingCartAPI/Program.cs
@@ -16,9 +16,12 @@ IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddScoped<ICouponRepository, CouponRepository>();
 builder.Services.AddSingleton<IMessageBus, AzureServiceBusMessageBus>();
 
 builder.Services.AddControllers();
+builder.Services.AddHttpClient<ICouponRepository, CouponRepository>(u=>u.BaseAddress=
+    new Uri(builder.Configuration["ServiceUrls:CouponAPI"]));
 
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer("Bearer", options =>
